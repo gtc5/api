a516480e65e16e23ff7f0a75bdbc9b0c2a4fb3fd
Clive Chan <doobahead@gmail.com>
2016-10-22 23:00:11 +0000

    fix login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 let app = require("express")();
 let jwt = require("jsonwebtoken");
 let Database = require("../Database");
+let crypto = require("crypto");
 
 function auth(collection, username, password){
   var db;
@@ -8,9 +9,21 @@ function auth(collection, username, password){
     db = _db;
     return db.collection(collection).find({username: username}).toArray();
   }).then(function(items){
-    if(!items || !items[0] || items[0].passhash != items[0].salt + password)
-      throw new Error("Incorrect username or password.");
-  }).then(genToken)
+    return new Promise(function(resolve, reject){
+      console.log(username, password, items);
+      if(!items || !items[0])
+        reject("Incorrect username or password.");
+      else{
+        console.log(items[0]);
+        crypto.pbkdf2(password, items[0].salt, 1000, 512, "sha1", function(err, derivedKey) {
+          if(err) reject(err);
+          else if(items[0].passhash != derivedKey.toString("hex")) reject("Incorrect username or password." + JSON.stringify(items[0]) + derivedKey.toString("hex") + password);
+          else resolve();
+        });
+      }
+    });
+  })
+  .then(genToken)
   .then(function(token){
     db.collection(collection).update({username: username}, {"$set": {token: token}});
     return token;
@@ -36,7 +49,7 @@ function checkToken(collection, token){
 app.get("/donor/auth", function(req, res, next){
   auth("donors", req.query.username, req.query.password)
   .then(function(token){res.send({token: token});})
-  .catch(function(){res.send({error: "Incorrect username or password."});});
+  .catch(function(err){console.error(err);res.send({error: "Incorrect username or password."});});
 });
 app.get("/donor/*", function(req, res, next){
   checkToken("donors", req.query.token).then(next).catch(function(){res.send({error: "Invalid token."});});
@@ -45,7 +58,7 @@ app.get("/donor/*", function(req, res, next){
 app.get("/volunteer/auth", function(req, res, next){
   auth("donors", req.query.username, req.query.password)
   .then(function(token){res.send({token: token});})
-  .catch(function(){res.send({error: "Incorrect username or password."});});
+  .catch(function(err){console.error(err);res.send({error: "Incorrect username or password."});});
 });
 app.get("/volunteer/*", function(req, res, next){
   checkToken("donors", req.query.token).then(next).catch(function(){res.send({error: "Invalid token."});});
