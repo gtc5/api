ec1a75b02f4056e55997c404360b60e4e8ce7fa2
Clive Chan <cc@clive.io>
2016-10-22 09:35:13 -0400

    Added mongodb integration

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
+let mongodb = require("mongodb");
+
+let MongoClient = mongodb.MongoClient;
+const url = "mongodb://localhost:27017/gtc5"
+const PORT = process.env.PORT || 33033;
+
+var db;
+MongoClient.connect(url).then(function(_db){
+  console.log("Connected to MongoDB");
+  db = _db;
+}).catch(function(err){
+  console.error(err);
+});
+
 
 app.use(bodyParser.urlencoded());
 
 app.get("/", function(req, res){
-  res.send("hello");
+  res.send("Second Harvest API Server");
 });
 
-app.post("/signin", function(){
-  //given a proper username and password, issues a token that the oth endpoints accept
+app.get("/volunteers", function(req, res){
+  db.collection("volunteers").find().toArray().then(function(result){
+    if(req.query.callback)
+      res.write(req.query.callback.replace(/[^\w\.]/g, '') + "(");
+    res.write(JSON.stringify(result));
+    if(req.query.callback)
+      res.write(");");
+    res.end();
+  }).catch(function(err){
+    console.error(err);
+  });
+});
+
+app.get("/donors", function(req, res){
+  db.collection("donors").find().toArray().then(function(result){
+    if(req.query.callback)
+      res.write(req.query.callback.replace(/[^\w\.]/g, '') + "(");
+    res.write(JSON.stringify(result));
+    if(req.query.callback)
+      res.write(");");
+    res.end();
+  }).catch(function(err){
+    console.error(err);
+  });
+});
+
+app.post("/signin", function(req, res){
+  [req.body.username]
+  //given a proper username and password, issues a token that the other endpoints accept
 });
 
 app.post("/post", function(req, res){
@@ -22,17 +63,17 @@ app.post("/claim", function(req, res){
   //Claims a request to execute
 });
 
-app.post("/pickedup", function(){
+app.post("/pickedup", function(req, res){
   req.body;
   //Notes that the item has been successfully picked up
 });
 
-app.post("/delivered", function(){
+app.post("/delivered", function(req, res){
   req.body;
   //Notes that the item has been successfully delivered
 });
 
-app.listen(process.env.PORT, function(){
-  console.log("Listening on :" + process.env.PORT);
+app.listen(PORT, function(){
+  console.log("Listening on :" + PORT);
 });
 
