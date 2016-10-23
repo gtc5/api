219a99f9c778094137301a2942b7b38faa8ddead
Clive Chan <doobahead@gmail.com>
2016-10-23 13:05:31 +0000

    db takes process.env.NODE_ENV, and starting to write tests

--- test/integration.js
+++ test/integration.js
@@ -1,5 +1,31 @@
+process.env.NODE_ENV = "test";
+process.env.PORT = 79079;
 
+let baseurl = "http://localhost:" + process.env.PORT;
+
+require("../initMongo");
+require("../server");
+
+let expect = require("chai").expect;
+let request = require("request");
 
 describe("auth", function(){
-  it("registers a user")
+  //case-insensitive username
+  //user flow with errors
+  //invalid username/password
+  
+  it("makes requests properly")
+  request(baseurl + "/donor/register?username=alexander&password=hamilton").then(function(res){
+    return request(baseurl + "/donor/auth?username=alexander&password=hamilton")
+    request(baseurl + "/donor/getdelivery?token=" + token)
+  
+  it("", function(done){
+    
+  });
+  it("generates a token", function(done){
+    
+  });
+  it("accepts the token", function(done){
+    
+  });
 });
