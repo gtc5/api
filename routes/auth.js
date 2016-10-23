555cd052a82ba1ec5ac5711d93da7805d3cb78aa
Clive Chan <doobahead@gmail.com>
2016-10-23 02:41:04 +0000

    add registration endpoints (open for now)

--- routes/auth.js
+++ routes/auth.js
@@ -3,23 +3,27 @@ let jwt = require("jsonwebtoken");
 let Database = require("../Database");
 let crypto = require("crypto");
 
+function hash(password, salt){
+  return new Promise(function(resolve,reject){
+    crypto.pbkdf2(password, salt, 1000, 128, "sha1", function(err, derivedKey) {
+      if(err) reject(err);
+      else resolve(derivedKey);
+    });
+  });
+}
+
 function auth(collection, username, password){
   var db;
   return Database.then(function(_db){
     db = _db;
     return db.collection(collection).find({username: username}).toArray();
-  }).then(function(items){
-    return new Promise(function(resolve, reject){
-      if(!items || !items[0])
-        reject("Incorrect username or password.");
-      else{
-        crypto.pbkdf2(password, items[0].salt, 1000, 128, "sha1", function(err, derivedKey) {
-          if(err) reject(err);
-          else if(items[0].passhash != derivedKey.toString("hex")) reject("Incorrect username or password." + JSON.stringify(items[0]) + derivedKey.toString("hex") + password);
-          else resolve();
-        });
-      }
-    });
+  })
+  .then(function(items){
+    if(!items || !items[0]) throw new Error;
+    return hash(password, items[0].salt);
+  })
+  .then(function(derivedKey){
+    if(items[0].passhash != derivedKey.toString("hex")) throw new Error;
   })
   .then(genToken)
   .then(function(token){
@@ -40,23 +44,54 @@ function checkToken(collection, token){
     return db.collection(collection).find({token: token}).toArray();
   }).then(function(items){
     if(!token || !items || !items[0] || items[0].token != token)
-      throw new Error("Invalid token.");
+      throw new Error;
+  });
+}
+function createAccount(collection, entry, req, res){
+  entry.username = req.query.username;
+  //TODO: verification that the input is well-formed
+  // - non duplicate username, valid username, password
+  
+  return genToken().then(function(token){
+    entry.token = token;
+    return hash(req.query.password, token);
+  }).then(function(passhash){
+    entry.passhash = passhash;
+    return Database;
+  }).then(function(db){
+    return db.collection("donors").insert(entry);
+  }).then(function(inserted){
+    res.send({id: entry._id});
+  }).catch(function(err){
+    res.send({error: err});
   });
 }
 
-app.get("/donor/auth", function(req, res, next){
+app.get("/donor/auth", function(req, res){
   auth("donors", req.query.username, req.query.password)
   .then(function(token){res.send({token: token});})
   .catch(function(err){console.error(err);res.send({error: "Incorrect username or password."});});
+});
+app.get("/donor/register", function(req, res){
+  var entry = {};
+  entry.name = req.query.name;
+  entry.location = req.query.location;
+  createAccount("donors", entry, req, res);
 });
 app.get("/donor/*", function(req, res, next){
   checkToken("donors", req.query.token).then(next).catch(function(){res.send({error: "Invalid token."});});
 });
 
-app.get("/volunteer/auth", function(req, res, next){
+app.get("/volunteer/auth", function(req, res){
   auth("donors", req.query.username, req.query.password)
   .then(function(token){res.send({token: token});})
   .catch(function(err){console.error(err);res.send({error: "Incorrect username or password."});});
+});
+app.get("/volunteer/register", function(req, res){
+  var entry = {};
+  entry.name = req.query.name;
+  entry.availability = req.query.availability;
+  createAccount("volunteers", entry, req, res);
 });
 app.get("/volunteer/*", function(req, res, next){
   checkToken("donors", req.query.token).then(next).catch(function(){res.send({error: "Invalid token."});});
